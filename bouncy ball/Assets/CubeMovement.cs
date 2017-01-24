b7914517ec79bac5de3bdb8d97965eb90a59fc99
fish <lucas.van.gastel@gmail.com>
2017-01-24 11:23:24 +0100

    cloned project, removed game builds and added project

---
+++ bouncy ball/Assets/CubeMovement.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeMovement : MonoBehaviour
+{
+        public float m_Radius;
+        private float m_Owner;
+        public float m_Speed = 5.0f;
+        public float m_MaxSpeed = 6000;
+        public Vector3 m_Normalized;
+        public Camera m_Cam;
+        private float m_Height;
+        private float m_Width;
+
+        public Vector3 NormallizeVectors(Vector3 one, Vector3 two)
+        {
+            Vector3 three = one - two;
+            float c = Mathf.Sqrt((three.x * three.x) + (three.y * three.y));
+            float Normalx = three.x / c;
+            float Normaly = three.y / c;
+            return new Vector3(Normalx, Normaly, 0);
+        }
+
+        void Update()
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+
+            m_Height = Camera.main.orthographicSize;
+            m_Width = m_Height * Camera.main.aspect;
+
+            for (int i = 0; i < m_Speed; i++)
+            {
+                transform.position += m_Normalized * Time.deltaTime;
+
+                if (transform.position.y > m_Height || transform.position.y < -m_Height)
+                {
+                    m_Normalized.y = m_Normalized.y * -1;
+                    m_Speed = Mathf.Clamp(m_Speed + 2f, 0, m_MaxSpeed);
+                }
+
+                if (transform.position.x > m_Width || transform.position.x < -m_Width)
+                {
+                    m_Normalized.x = m_Normalized.x * -1;
+
+                    m_Speed = Mathf.Clamp(m_Speed + 2f, 0, m_MaxSpeed);
+                }
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector3 clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                m_Normalized = NormallizeVectors(clickPosition, transform.position);
+            }
+        }
+
+        public void Collision()
+        {
+            GetComponent<Renderer>().material.color = Color.cyan;
+        }
+    }
---
+++ bouncy ball/Assets/Operator.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Operator : MonoBehaviour
+{
+    [SerializeField]
+    private List<moceToMouse> m_Balls = new List<moceToMouse>();
+
+    [SerializeField]
+    private List<CubeMovement> m_Box = new List<CubeMovement>();
+
+    void Start()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+        //ball collision
+        for (int i = 0; i < m_Balls.Count; i++)
+        {
+            Vector3 ball1 = m_Balls[i].transform.position;
+            float radius1 = m_Balls[i].m_Radius;
+
+            for (int j = 0; j < m_Balls.Count; j++)
+            {
+                Vector3 ball2 = m_Balls[j].transform.position;
+                float radius2 = m_Balls[j].m_Radius;
+
+                bool collision = false;
+                if (i != j)
+                {
+                    collision = CircleCollisions(ball1, ball2, radius1, radius2);
+                }
+
+                if (collision == true)
+                {
+                    m_Balls[i].Collision();
+                    m_Balls[j].Collision();
+                }
+            }
+        }
+        //box in box check
+        for (int i = 0; i < m_Box.Count; i++)
+        {
+            Vector3 box1 = m_Box[i].transform.position;
+            float RightSide1 = m_Box[i].transform.localScale.x / 2;
+            float TopSide1 = m_Box[i].transform.localScale.y / 2;
+
+            for (int j = 0; j < m_Box.Count; j++)
+            {
+                Vector3 box2 = m_Box[j].transform.position;
+                float RightSide2 = m_Box[j].transform.localScale.x / 2;
+                float TopSide2 = m_Box[j].transform.localScale.x / 2;
+
+                bool collision = false;
+                if (i != j)
+                {
+                    collision = BoxInBoxCollision(box1, box2, RightSide1, TopSide1, RightSide2, TopSide2);
+                }
+
+                if (collision == true)
+                {
+                    m_Box[i].Collision();
+                    m_Box[j].Collision();
+                }
+            }
+        }
+    }
+
+    bool CircleCollisions(Vector3 one, Vector3 two, float radius1, float radius2)
+    {
+        float awnser = Mathf.Sqrt((one.x - two.x) * (one.x - two.x) + (one.y - two.y) * (one.y - two.y));
+        float radiusdistance = radius1 + radius2;
+
+        bool collision = false;
+        if (awnser <= radiusdistance)
+        {
+            collision = true;
+        }
+
+        return collision;
+    }
+
+    bool BoxInBoxCollision(Vector3 one, Vector3 two, float rightSide1, float topSide1, float rightSide2, float topSide2)
+    {
+        float lowestSide1 = -topSide1;
+        float leftSide1 = -rightSide1;
+        float lowestSide2 = -topSide2;
+        float leftSide2 = -rightSide2;
+
+        bool collision = false;
+
+        if (one.x + rightSide1 > two.x + leftSide2 && one.x + leftSide1 < two.x + rightSide2 && one.y + lowestSide1 < two.y + topSide2 && one.y + topSide1 > two.y + lowestSide2)
+        {
+            collision = true;
+        }
+        
+
+
+
+        return collision;
+    }
+}
+
